04f5a0c0a2f764f87a49d9565aa9984c993705c0
hbl25@hotmail.fr <IudexPanzyr>
2025-12-26 16:10:23 +0100

    Add jerseys URLs migration

---
+++ worker/migrations/0006_jerseys_urls.sql
@@ -0,0 +1,25 @@
+-- Auto-generated from data/jerseys.csv
+-- Base (if needed): https://iudexpanzyr.github.io/QuizCyclisme/jerseys/
+-- Expected columns: name,file (separator can be ',' or ';')
+
+UPDATE teams SET jersey_url='https://iudexpanzyr.github.io/QuizCyclisme/jerseys/Alpecin.png' WHERE name='Alpecin-Premier Tech';
+UPDATE teams SET jersey_url='https://iudexpanzyr.github.io/QuizCyclisme/jerseys/Bahrain.png' WHERE name='Bahrain - Victorious';
+UPDATE teams SET jersey_url='https://iudexpanzyr.github.io/QuizCyclisme/jerseys/Decathlon.png' WHERE name='Decathlon CMA CGM Team';
+UPDATE teams SET jersey_url='https://iudexpanzyr.github.io/QuizCyclisme/jerseys/EF.png' WHERE name='EF Education - EasyPost';
+UPDATE teams SET jersey_url='https://iudexpanzyr.github.io/QuizCyclisme/jerseys/FDJ.png' WHERE name='Groupama - FDJ United';
+UPDATE teams SET jersey_url='https://iudexpanzyr.github.io/QuizCyclisme/jerseys/Ineos.png' WHERE name='INEOS Grenadiers';
+UPDATE teams SET jersey_url='https://iudexpanzyr.github.io/QuizCyclisme/jerseys/Trek.png' WHERE name='Lidl - Trek';
+UPDATE teams SET jersey_url='https://iudexpanzyr.github.io/QuizCyclisme/jerseys/Lotto.png' WHERE name='Lotto Intermarché';
+UPDATE teams SET jersey_url='https://iudexpanzyr.github.io/QuizCyclisme/jerseys/Movistar.png' WHERE name='Movistar Team';
+UPDATE teams SET jersey_url='https://iudexpanzyr.github.io/QuizCyclisme/jerseys/Nsm.png' WHERE name='NSN Cycling Team';
+UPDATE teams SET jersey_url='https://iudexpanzyr.github.io/QuizCyclisme/jerseys/Bora.png' WHERE name='Red Bull - BORA - hansgrohe';
+UPDATE teams SET jersey_url='https://iudexpanzyr.github.io/QuizCyclisme/jerseys/Soudal.png' WHERE name='Soudal Quick-Step';
+UPDATE teams SET jersey_url='https://iudexpanzyr.github.io/QuizCyclisme/jerseys/Jayco.png' WHERE name='Team Jayco AlUla';
+UPDATE teams SET jersey_url='https://iudexpanzyr.github.io/QuizCyclisme/jerseys/PostNL.png' WHERE name='Team Picnic PostNL';
+UPDATE teams SET jersey_url='https://iudexpanzyr.github.io/QuizCyclisme/jerseys/Visma.png' WHERE name='Team Visma Lease a Bike';
+UPDATE teams SET jersey_url='https://iudexpanzyr.github.io/QuizCyclisme/jerseys/UAE.png' WHERE name='UAE Team Emirates - XRG';
+UPDATE teams SET jersey_url='https://iudexpanzyr.github.io/QuizCyclisme/jerseys/UnoX.png' WHERE name='Uno-X Mobility';
+UPDATE teams SET jersey_url='https://iudexpanzyr.github.io/QuizCyclisme/jerseys/Astana.png' WHERE name='XDS Astana Team';
+
+-- Rows generated: 18
+-- Rows skipped: 0
